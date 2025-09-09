b84e782891dbe53e513b8d8c44ce2df560e1b18f
Ashutosh maurya <ashutoshmauriyacse@gmail.com>
2025-09-10 00:51:11 +0530

    deployment changes

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://nextround-uo4n.onrender.com",
   credentials: true
 }));
 
